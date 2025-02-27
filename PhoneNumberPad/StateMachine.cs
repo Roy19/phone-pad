08a33261d56130856ba6391e91e634e44a7cafb8
Roy19 <aritra.surroy@acquia.com>
2025-02-27 18:41:53 +0530

    logic for state machine

--- PhoneNumberPad/StateMachine.cs
+++ PhoneNumberPad/StateMachine.cs
@@ -5,9 +5,28 @@ namespace PhoneNumberPad;
 public class StateMachine
 {
     private Stack<PhoneChar> stack;
+    private LookupTable lookupTable;
 
-    public StateMachine()
+    public StateMachine(LookupTable lookupTable)
     {
+        this.lookupTable = lookupTable;
         stack = new Stack<PhoneChar>();
+    }
+
+    public void ProcessCharacter(PhoneChar phoneChar)
+    {
+        PhoneChar lastChar = stack.Count > 0 ? stack.Peek() : phoneChar;
+        if (phoneChar.Symbol == lastChar.Symbol)
+        {
+            stack.Push(phoneChar);
+        } else {
+            string typedString = "";
+            while (stack.Count > 0 && !stack.Peek().IsAlphabet())
+            {
+                typedString = stack.Pop().Symbol + typedString;
+            }
+            PhoneChar currentChar = lookupTable.Convert(typedString);
+            stack.Push(currentChar);
+        }
     }
 }
